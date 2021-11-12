0bdfbc5f1748f255589efd7196fdc37f4c949c80
¨leodelgadodev¨ <leodelgado.dev@gmail.com>
2021-11-12 20:38:56 -0300

    btn working properly, better styles and floating text bubble

--- Content.js
+++ Content.js
@@ -1,25 +1,54 @@
+/**
+ * Wait for an element before resolving a promise
+ * @param {String} querySelector - Selector of element to wait for
+ * @param {Integer} timeout - Milliseconds to wait before timing out, or 0 for no timeout              
+ */
+ function waitForElement(querySelector, timeout=0) {
+    const startTime = new Date().getTime();
+    return new Promise((resolve, reject)=>{
+        const timer = setInterval(()=>{
+            const now = new Date().getTime();
+            if(document.querySelector(querySelector)){
+                clearInterval(timer);
+                resolve();
+            }else if(timeout && now - startTime >= timeout){
+                clearInterval(timer);
+                reject();
+            }
+        }, 200);
+    });
+}
+
+
 let lastUrl = location.href; 
 
-new MutationObserver(() => {
-  const url = location.href;
-  if (url !== lastUrl) {
-    lastUrl = url;
-    onUrlChange();
-  }
-}).observe(document, {subtree: true, childList: true});
+waitForElement(".pr-header-branches", 9000).then(() => {
+    new MutationObserver(() => {
+        const url = location.href;
+        if (url !== lastUrl) {
+          lastUrl = url;
+          onUrlChange();
+        }
+      }).observe(document, {subtree: true, childList: true});
+      
+      if (hasReloadedPage()) {
+          onUrlChange();
+      }
+})
 
-if (hasReloadedPage()) {
-    onUrlChange();
-}
 
 function hasReloadedPage() {
     return window.performance.getEntriesByType("navigation") && window.performance.getEntriesByType("navigation")[0].type === 'reload';
 }
 
+function isButtonRendered() {
+    return !!document.getElementById("azure-utils-ext-src-branch-btn");
+}
+
 function onUrlChange() {
     try {
         const isPullRequestURL = location.href.split('/').some((urlSection) => urlSection === 'pullrequest');
-        if (isPullRequestURL) {
+        if (isPullRequestURL && !isButtonRendered()) {
             addPullRequestButtons();
         }
     } catch (error) {
@@ -29,38 +58,39 @@ function onUrlChange() {
 
 
 function addPullRequestButtons() {
-    let PRHeaderHTML = document.getElementsByClassName("pr-header-branches")[0]
-    PRHeaderHTML.children[0].insertAdjacentHTML('afterEnd','<div id="azure-utils-ext-src-branch-btn">Copy</div>');
-    PRHeaderHTML.children[PRHeaderHTML.children.length-1].insertAdjacentHTML('afterEnd','<div id="azure-utils-ext-target-branch-btn">Copy</div>');
+    const buttonHTML = (tagId, branchType) => {
+    return `<div id=${tagId} title="Copy ${branchType} branch to clipboard" class="bolt-clipboard-button hash-copy-button repos-commit-header-hash"><button aria-label="Copy to clipboard" class="bolt-button bolt-icon-button enabled subtle icon-only bolt-focus-treatment" data-focuszone="" data-is-focusable="true" role="button" tabindex="0" type="button"><span aria-hidden="true" class="left-icon flex-noshrink fabric-icon ms-Icon--Copy medium"></span></button></div>`
+    } 
 
-    let srcBranchBtnElement = document.getElementById("azure-utils-ext-src-branch-btn");
-    let targetBranchBtnElement = document.getElementById("azure-utils-ext-target-branch-btn");
+    const buttonTextHoverHTML = (tagId, branchType) => {
+        return `<div id=${tagId} class="bolt-tooltip bolt-callout absolute flex-row" id="__bolt-tooltip-24" role="tooltip" style="left: 5%; top: 5%; position: absolute; z-index: 9;"><div class="bolt-callout-content" role="tooltip"><div class="bolt-tooltip-content body-m">Copied ${branchType} to Clipboard!</div></div></div>`
+    }
 
-    let style_normal = "margin: 0px 4px 0px 3px; width: 40px; display: inline; padding: 2px 3px; background-color: var(--palette-black-alpha-6,rgba(0, 0, 0, 0.06)); color: var(--text-primary-color,rgba(0, 0, 0, 0.9)); cursor: pointer;"
-    let style_hover = "margin: 0px 4px 0px 3px; width: 40px; display: inline; padding: 2px 3px; background-color: var(--palette-black-alpha-10,rgba(0, 0, 0, .1)); color: var(--text-primary-color,rgba(0, 0, 0, 0.9)); cursor: pointer;"
-    let style_click = "margin: 0px 4px 0px 3px; width: 40px; display: inline; padding: 2px 3px; background-color: rgba(85,163,98,1) !important; color: var(--text-primary-color,rgba(0, 0, 0, 0.9)); cursor: pointer;";
+    const PR_HeaderElement = document.getElementsByClassName("pr-header-branches")[0]
+    PR_HeaderElement.children[0].insertAdjacentHTML('afterEnd',buttonHTML("azure-utils-ext-src-branch-btn", "source"));
+    PR_HeaderElement.children[PR_HeaderElement.children.length-1].insertAdjacentHTML('afterEnd',buttonHTML("azure-utils-ext-target-branch-btn", "target"));
 
-    function configureBtnForBranch(aBranch, prHeaderChild) {
-        aBranch.style = style_normal; 
+    const srcBranchBtnElement = document.getElementById("azure-utils-ext-src-branch-btn");
+    const targetBranchBtnElement = document.getElementById("azure-utils-ext-target-branch-btn");
+    const btnCustomStyles = "margin: 0px 4px 0px 3px; width: 40px; display: inline; padding: 2px 3px; color: var(--text-primary-color,rgba(0, 0, 0, 0.9)); cursor: pointer;"
+
+    function configureBtnForBranch(aBranch, prHeaderChild, branchType) {
+        aBranch.style = btnCustomStyles; 
+        const prHeaderContainer = document.getElementsByClassName("repos-pr-header")[0];
         
-        aBranch.addEventListener('mouseenter', e => {
-            e.currentTarget.style = style_hover;
-        });
-        
-        aBranch.addEventListener('mouseleave', e => {
-            e.currentTarget.style = style_normal;
-        });
-        
-        aBranch.addEventListener('click', e => {
-            e.currentTarget.style = style_click;
-            aBranch.innerHTML = "Copied!";
-            navigator.clipboard.writeText(
-                PRHeaderHTML.children[prHeaderChild].innerHTML
-            )
+        aBranch.addEventListener('click', _ => {
+            prHeaderContainer.children[0].insertAdjacentHTML('afterEnd', 
+                buttonTextHoverHTML("azure-utils-ext-src-branch-btn-msg", branchType)
+            );
+            
+            navigator.clipboard.writeText(PR_HeaderElement.children[prHeaderChild].innerHTML);
+            
             setTimeout(() => {
-                aBranch.innerHTML = "Copy";
-                aBranch.style = style_normal;
-            } , 1500)
+                document.getElementById("azure-utils-ext-src-branch-btn-msg").style = "left: 5%; top: 5%; position: absolute; z-index: 9;  animation: tooltip-fade-out 300ms ease-in;";
+                setTimeout(() => {
+                    document.getElementById("azure-utils-ext-src-branch-btn-msg").remove();
+                }, 300)
+            }, 900)
         });
     }
 
@@ -70,6 +100,6 @@ function addPullRequestButtons() {
         }
     })
 
-    configureBtnForBranch(srcBranchBtnElement, 0);
-    configureBtnForBranch(targetBranchBtnElement, PRHeaderHTML.children.length-2);
+    configureBtnForBranch(srcBranchBtnElement, 0, "source");
+    configureBtnForBranch(targetBranchBtnElement, PR_HeaderElement.children.length-2, "target");
 }
