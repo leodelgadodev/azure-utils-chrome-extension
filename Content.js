08013c6b240edc890fdced1275659e75e9bac884
¨leodelgadodev¨ <leodelgado.dev@gmail.com>
2021-11-14 21:37:19 -0300

    position text hover relative to button and prevent text hover spam

--- Content.js
+++ Content.js
@@ -1,9 +1,12 @@
+// import { waitForElement } from "./Utils";
+// import { azureUtilsExtState } from "./State.mjs";
+
 /**
- * Wait for an element before resolving a promise
- * @param {String} querySelector - Selector of element to wait for
- * @param {Integer} timeout - Milliseconds to wait before timing out, or 0 for no timeout              
- */
- function waitForElement(querySelector, timeout=0) {
+* Wait for an element before resolving a promise
+* @param {String} querySelector - Selector of element to wait for
+* @param {Integer} timeout - Milliseconds to wait before timing out, or 0 for no timeout              
+*/
+function waitForElement(querySelector, timeout=0) {
     const startTime = new Date().getTime();
     return new Promise((resolve, reject)=>{
         const timer = setInterval(()=>{
@@ -17,11 +20,25 @@
             }
         }, 200);
     });
+ }
+
+const State = {
+    BtnBranchBtn_isTextHoverActive: {
+        source: false,
+        target: false
+    },
+    isTextHoverActive: (branchType) => {
+        return State.BtnBranchBtn_isTextHoverActive[branchType];
+    },
+    setTextHoverAsActive: (branchType) => {
+        State.BtnBranchBtn_isTextHoverActive[branchType] = true;
+    },
+    setTextHoverAsInative: (branchType) => {
+        State.BtnBranchBtn_isTextHoverActive[branchType] = false;
+    }
 }
 
-
 let lastUrl = location.href; 
-
 waitForElement(".pr-header-branches", 9000).then(() => {
     new MutationObserver(() => {
         const url = location.href;
@@ -35,7 +52,6 @@ waitForElement(".pr-header-branches", 9000).then(() => {
           onUrlChange();
       }
 })
-
 
 function hasReloadedPage() {
     return window.performance.getEntriesByType("navigation") && window.performance.getEntriesByType("navigation")[0].type === 'reload';
@@ -58,39 +74,43 @@ function onUrlChange() {
 
 
 function addPullRequestButtons() {
-    const buttonHTML = (tagId, branchType) => {
-    return `<div id=${tagId} title="Copy ${branchType} branch to clipboard" class="bolt-clipboard-button hash-copy-button repos-commit-header-hash"><button aria-label="Copy to clipboard" class="bolt-button bolt-icon-button enabled subtle icon-only bolt-focus-treatment" data-focuszone="" data-is-focusable="true" role="button" tabindex="0" type="button"><span aria-hidden="true" class="left-icon flex-noshrink fabric-icon ms-Icon--Copy medium"></span></button></div>`
+    const buttonHTML = (branchType) => {
+    return `<div id="azure-utils-ext-${branchType}-branch-btn" style="position: relative;" title="Copy ${branchType} branch to clipboard" class="bolt-clipboard-button hash-copy-button repos-commit-header-hash"><button aria-label="Copy to clipboard" class="bolt-button bolt-icon-button enabled subtle icon-only bolt-focus-treatment" data-focuszone="" data-is-focusable="true" role="button" tabindex="0" type="button"><span aria-hidden="true" class="left-icon flex-noshrink fabric-icon ms-Icon--Copy medium">${buttonTextHoverHTML(branchType)}</span></button></div>`
     } 
 
-    const buttonTextHoverHTML = (tagId, branchType) => {
-        return `<div id=${tagId} class="bolt-tooltip bolt-callout absolute flex-row" id="__bolt-tooltip-24" role="tooltip" style="left: 5%; top: 5%; position: absolute; z-index: 9;"><div class="bolt-callout-content" role="tooltip"><div class="bolt-tooltip-content body-m">Copied ${branchType} to Clipboard!</div></div></div>`
+    const buttonTextHoverHTML = (branchType) => {
+        return `<div id="azure-utils-ext-${branchType}-branch-btn-text-hover" class="bolt-tooltip bolt-callout absolute flex-row" id="__bolt-tooltip-24" style="display: none;" role="tooltip"><div class="bolt-callout-content" role="tooltip"><div class="bolt-tooltip-content body-m">Copied ${branchType} to Clipboard!</div></div></div>`
     }
 
     const PR_HeaderElement = document.getElementsByClassName("pr-header-branches")[0]
-    PR_HeaderElement.children[0].insertAdjacentHTML('afterEnd',buttonHTML("azure-utils-ext-src-branch-btn", "source"));
-    PR_HeaderElement.children[PR_HeaderElement.children.length-1].insertAdjacentHTML('afterEnd',buttonHTML("azure-utils-ext-target-branch-btn", "target"));
+    PR_HeaderElement.children[0].insertAdjacentHTML('afterEnd',buttonHTML("source"));
+    PR_HeaderElement.children[PR_HeaderElement.children.length-1].insertAdjacentHTML('afterEnd',buttonHTML("target"));
 
-    const srcBranchBtnElement = document.getElementById("azure-utils-ext-src-branch-btn");
+    const srcBranchBtnElement = document.getElementById("azure-utils-ext-source-branch-btn");
     const targetBranchBtnElement = document.getElementById("azure-utils-ext-target-branch-btn");
-    const btnCustomStyles = "margin: 0px 4px 0px 3px; width: 40px; display: inline; padding: 2px 3px; color: var(--text-primary-color,rgba(0, 0, 0, 0.9)); cursor: pointer;"
-
+    const btnCustomStyles = "margin: 0px 4px 0px 3px; width: 40px; display: inline; padding: 2px 3px; color: var(--text-primary-color,rgba(0, 0, 0, 0.9)); cursor: pointer;";
+    const buttonTextHoverCSS = "white-space: nowrap; left: 1em; bottom: -1em; position: absolute; z-index: 9; vertical-align: middle; line-height: normal;";
+    
     function configureBtnForBranch(aBranch, prHeaderChild, branchType) {
         aBranch.style = btnCustomStyles; 
-        const prHeaderContainer = document.getElementsByClassName("repos-pr-header")[0];
-        
+
         aBranch.addEventListener('click', _ => {
-            prHeaderContainer.children[0].insertAdjacentHTML('afterEnd', 
-                buttonTextHoverHTML("azure-utils-ext-src-branch-btn-msg", branchType)
-            );
-            
-            navigator.clipboard.writeText(PR_HeaderElement.children[prHeaderChild].innerHTML);
-            
-            setTimeout(() => {
-                document.getElementById("azure-utils-ext-src-branch-btn-msg").style = "left: 5%; top: 5%; position: absolute; z-index: 9;  animation: tooltip-fade-out 300ms ease-in;";
+            if (!State.isTextHoverActive(branchType)) {
+                State.setTextHoverAsActive(branchType);
+                document.getElementById(`azure-utils-ext-${branchType}-branch-btn-text-hover`).style = `${buttonTextHoverCSS} animation: tooltip-fade-in 300ms ease-in;`;
+    
+                if (PR_HeaderElement.children[prHeaderChild]) {
+                    navigator.clipboard.writeText(PR_HeaderElement.children[prHeaderChild].innerHTML);
+                }
+                
                 setTimeout(() => {
-                    document.getElementById("azure-utils-ext-src-branch-btn-msg").remove();
-                }, 300)
-            }, 900)
+                    document.getElementById(`azure-utils-ext-${branchType}-branch-btn-text-hover`).style = `${buttonTextHoverCSS} animation: tooltip-fade-out 300ms ease-in;`;
+                    setTimeout(() => {
+                        document.getElementById(`azure-utils-ext-${branchType}-branch-btn-text-hover`).style = "display: none;";
+                        State.setTextHoverAsInative(branchType);
+                    }, 300)
+                }, 900)
+            }
         });
     }
 
@@ -98,7 +118,7 @@ function addPullRequestButtons() {
         if (e.ctrlKey && e.key === 'b') {
             srcBranchBtnElement.click();
         }
-    })
+    });
 
     configureBtnForBranch(srcBranchBtnElement, 0, "source");
     configureBtnForBranch(targetBranchBtnElement, PR_HeaderElement.children.length-2, "target");
